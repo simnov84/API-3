9638e5be1b1db67c58a2dc81d1de3d161650b3b7
simnov84 <83582907+simnov84@users.noreply.github.com>
2021-08-01 01:56:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+async function fetchdata() {
+    const users = await fetch(
+      "http://makeup-api.herokuapp.com/api/v1/products.json",
+      
+      {
+        method: "GET"
+      }
+    );
+    const makeup = await users.json();
+    loaddata(makeup);
+  }
+  function loaddata(makeup) {
+    const product = document.createElement("div");
+    //product.className = "productList";
+    makeup.forEach((user) => {
+      const usercontainer = document.createElement("div");
+      usercontainer.className = "container";
+      usercontainer.innerHTML = `
+      
+      <img src="${user.image_link}" class="center">
+      <div><span>Name:</span> ${user.name}</div><br>
+      <div><span>Brand:</span> ${user.brand}</div><br>
+      <div><span>Price:</span> ${user.price}</div><br>
+      
+      <p>${user.description}</p><br>
+      <a href="${user.product_link}" target ="_blank">Click here</a><br>
+    `;
+      product.append(usercontainer);
+    });
+    document.body.append(product);
+  }
+  fetchdata();
